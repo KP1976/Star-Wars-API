d4b4204a8f12e35ddbb2193dfe899487e1a18914
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-06-29 17:53:51 +0200

    Add sass-loader, node-sass, style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,26 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'final.js',
 	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader', // creates style nodes from JS strings
+					'css-loader', // translates CSS into CommonJS
+					'sass-loader', // compiles Sass to CSS
+				],
+			},
+		],
+	},
 };
