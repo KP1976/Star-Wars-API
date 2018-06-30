a20d3e022edd9182a7a4d5b63ded885166c8c55f
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-06-30 10:29:07 +0200

    Install axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
 import './scss/style.scss';
+
+axios.get('https://swapi.co/api/')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
