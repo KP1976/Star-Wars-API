54a84f9c6de7a2a07bed01bca9853f2b9959e3bd
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-06-30 13:54:05 +0200

    Show results (people) from Api

--- src/js/results.js
+++ src/js/results.js
@@ -7,5 +7,18 @@ const item = data => `<li class="search-list__item">${data}</li>`;
 export const result = (option, data) => {
 	let html;
 
+	switch (option) {
+		case 'people':
+			html = data.map(result =>
+				item(`Name: <strong>${result.name}</strong>, 
+      Height: <strong>${result.height}</strong>, Hair Color: <strong>${result.hair_color}`),
+			);
+			break;
+		case 'planets':
+			break;
+		default:
+			return;
+	}
+
 	searchList.innerHTML = html;
 };
