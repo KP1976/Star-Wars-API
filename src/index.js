bd0b2837b15344d7a46312c7d7a64383144ffb0b
Krzysztof Pawlowski <kpawlowski1976@gmail.com>
2018-06-30 10:39:07 +0200

    Add variables of DOM elements and base url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import './scss/style.scss';
 
-axios.get('https://swapi.co/api/')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// Get DOM elements
+const searchForm = document.querySelector('.form');
+const formInput = document.querySelector('.form__input');
+const formSelect = document.querySelector('.form__select');
+const searchList = document.querySelector('.search-list');
+
+console.log(searchForm, formInput, formSelect, searchList);
+
+// axios
+// 	.get('https://swapi.co/api/')
+// 	.then(response => console.log(response))
+// 	.catch(error => console.log(error));
